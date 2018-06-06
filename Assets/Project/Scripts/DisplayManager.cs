fe8bec6be7d6268cb5bf212c9e58fba48e1e02d6
Daniel Rousseau <danrousseau@protonmail.com>
2018-06-06 12:18:05 +0200

    Adding my VR project to git.

---
+++ Assets/Project/Scripts/DisplayManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DisplayManager : MonoBehaviour
+{
+    private void Start()
+    {
+        OVRManager.tiledMultiResLevel = OVRManager.TiledMultiResLevel.LMSHigh;
+        OVRManager.display.displayFrequency = 10.0f;
+    }
+}
---
+++ Assets/Project/Scripts/RaycastTeleport.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastTeleport : MonoBehaviour
+{
+    [SerializeField] private LayerMask mask;
+    [SerializeField] private Transform rayPoint;
+
+    private void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayPoint.position, rayPoint.TransformDirection(Vector3.forward), out hitInfo,
+                Mathf.Infinity, mask))
+            {
+                if (hitInfo.collider.tag == "Point")
+                {
+                    this.gameObject.transform.position = hitInfo.collider.gameObject.transform.position;
+                    this.gameObject.transform.rotation = hitInfo.collider.gameObject.transform.rotation;
+                }
+            }
+        }
+    }
+}
