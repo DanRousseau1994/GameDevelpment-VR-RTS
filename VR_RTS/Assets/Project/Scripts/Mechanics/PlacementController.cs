bfb952280f4048f177e226b5b7f02f835a7a73d9
LabBeast <ricktreweek@gmail.com>
2018-06-14 17:49:33 +0200

    Added the menus and UI's, started work on resource system and intergrated it with building system

---
+++ VR_RTS/Assets/Project/Scripts/Mechanics/PlacementController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class PlacementController : MonoBehaviour
+{
+    [SerializeField] private GameObject placeableObjectPrefab;
+
+    private GameObject currentPlaceableObject;
+
+    private float mouseWheelRotation;
+
+    public Transform rayPoint;
+    [SerializeField] private LayerMask _LayerMask;
+    [SerializeField] private Grid grid;
+
+    private void Update()
+    {
+        HandleNewObjectHotkey();
+
+        if (currentPlaceableObject != null)
+        {
+            MoveCurrentObjectToMouse();
+            RotateFromMouseWheel();
+            ReleaseIfClicked();
+        }
+    }
+
+    private void HandleNewObjectHotkey()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            if (currentPlaceableObject != null)
+            {
+                Destroy(currentPlaceableObject);
+            }
+            else
+            {
+                currentPlaceableObject = Instantiate(placeableObjectPrefab);
+            }
+        }
+    }
+
+    private void MoveCurrentObjectToMouse()
+    {
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(rayPoint.position, rayPoint.TransformDirection(Vector3.forward), out hitInfo, Mathf.Infinity, _LayerMask))
+        {
+            currentPlaceableObject.transform.position = hitInfo.point;
+            currentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            currentPlaceableObject.transform.position = grid.GetNearestPointOnGrid(hitInfo.point);
+        }
+    }
+
+    private void RotateFromMouseWheel()
+    {
+        Debug.Log(Input.mouseScrollDelta);
+        mouseWheelRotation += Input.mouseScrollDelta.y;
+        currentPlaceableObject.transform.Rotate(Vector3.up, mouseWheelRotation * 10f);
+    }
+
+    private void ReleaseIfClicked()
+    {
+        if (Resourcesources.avaliableResources.UseResources(300) == false)
+            return;
+
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            currentPlaceableObject = null;  
+        }
+    }
+}
---
+++ VR_RTS/Assets/Project/Scripts/Menu&UI/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void ViewControls()
+    {
+        //Display control map here.
+    }
+}
